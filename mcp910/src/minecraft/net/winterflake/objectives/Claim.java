bd4aa6f57aa169ee77b09d21108da9d181b8e42c
leijurv <leijurv@gmail.com>
2015-03-27 18:01:04 -0700

    formatting

--- mcp910/src/minecraft/net/winterflake/objectives/Claim.java
+++ mcp910/src/minecraft/net/winterflake/objectives/Claim.java
@@ -14,13 +14,14 @@ import net.minecraft.item.ItemStack;
  * @author howardstark
  * @author ave4224
  */
-public class Claim{
+public class Claim {
 	
 	final Need need;
 	private final ItemStack item;
 	final AquireItemObjective aio;
-	private volatile boolean completed;//Helper variable, not completely needed
+	private volatile boolean completed;// Helper variable, not completely needed
 	private volatile int completion;
+	
 	/**
 	 * This is a <code>HashMap</code> of every outstanding claim of items.
 	 */
@@ -32,70 +33,82 @@ public class Claim{
 		need = aio.need;
 		completion = 0;
 		registerClaim(this);
-		completed=false;
+		completed = false;
 	}
 	
-//	@Override
-//	public int compareTo(Claim o) {
-//		System.out.println("Comparing " + this + " to " + o);
-//		if(this.need != o.need){
-//			if(this.need == Need.MULTI)
-//				return 
-//		}
-//		return new Double(this.aio.getPriority()).compareTo(o.aio.getPriority());
-//		// Compare priorities not adjusted priorities, because adjusted
-//		// priorities are derived from completion percentage
-//	}
+	// @Override
+	// public int compareTo(Claim o) {
+	// System.out.println("Comparing " + this + " to " + o);
+	// if(this.need != o.need){
+	// if(this.need == Need.MULTI)
+	// return
+	// }
+	// return new Double(this.aio.getPriority()).compareTo(o.aio.getPriority());
+	// // Compare priorities not adjusted priorities, because adjusted
+	// // priorities are derived from completion percentage
+	// }
 	/**
 	 * How many items have been completed already
+	 * 
 	 * @return how many items have been completed already
 	 */
 	public int getAmountCompleted() {
 		return completion;
 	}
+	
 	/**
 	 * Get the item claimed
+	 * 
 	 * @return the item
 	 */
-	public Item getItem(){
+	public Item getItem() {
 		return item.getItem();
 	}
+	
 	/**
 	 * Get the stack size of the item claimed
+	 * 
 	 * @return the stack size
 	 */
-	public int getStackSize(){
+	public int getStackSize() {
 		return item.stackSize;
 	}
+	
 	/**
 	 * Is this claim completely fufilled?
 	 * 
 	 * @return whether it's finished
 	 */
-	public boolean isFinished(){
-		if(completion==item.stackSize){
-			completed=true;
+	public boolean isFinished() {
+		if (completion == item.stackSize) {
+			completed = true;
 			return true;
 		}
-		completed=false;
+		completed = false;
 		return false;
 	}
+	
 	/**
-	 * Called when this claim is fully or partially filled by items entering the inventory
+	 * Called when this claim is fully or partially filled by items entering the
+	 * inventory
 	 * 
-	 * @param amount The amount of items fufilled
-	 * @return The amount leftover after this claim takes what it can. A return of 0 means that this claim took all the fufilled items, a nonzero return means that some were leftover
+	 * @param amount
+	 *            The amount of items fufilled
+	 * @return The amount leftover after this claim takes what it can. A return
+	 *         of 0 means that this claim took all the fufilled items, a nonzero
+	 *         return means that some were leftover
 	 */
 	private int onFufill(int amount) {
 		int remaining = item.stackSize - completion;
-		if(amount <= remaining){
+		if (amount <= remaining) {
 			completion += amount;
 			return 0;
 		}
 		completion += remaining;
 		amount -= remaining;
-		completed=true;
-		// TODO remove self from list of outstanding claims, because I have been completely fufilled.
+		completed = true;
+		// TODO remove self from list of outstanding claims, because I have been
+		// completely fufilled.
 		return amount;
 	}
 	
@@ -112,10 +125,13 @@ public class Claim{
 	}
 	
 	/**
-	 * Get the claim with the highest priority in the queue for the given itemID. Multiple use claims will be returned first.
+	 * Get the claim with the highest priority in the queue for the given
+	 * itemID. Multiple use claims will be returned first.
 	 *
-	 * @param itemID the itemID
-	 * @return the highest priority multi claim then the highest priority single claim.
+	 * @param itemID
+	 *            the itemID
+	 * @return the highest priority multi claim then the highest priority single
+	 *         claim.
 	 */
 	public static Claim getHighestPriorityClaim(Item item) {
 		ArrayList<Claim> possibilities = claimList.get(item);
@@ -123,8 +139,8 @@ public class Claim{
 			return null;
 		System.out.println(possibilities);
 		Claim max = possibilities.get(0);
-		for(int i = 1; i < possibilities.size(); i++){
-			if((max.need == Need.SINGLE || possibilities.get(i).need == Need.MULTI) && possibilities.get(i).aio.getPriority() > max.aio.getPriority())
+		for (int i = 1; i < possibilities.size(); i++) {
+			if ((max.need == Need.SINGLE || possibilities.get(i).need == Need.MULTI) && possibilities.get(i).aio.getPriority() > max.aio.getPriority())
 				max = possibilities.get(i);
 		}
 		return max;
@@ -146,7 +162,7 @@ public class Claim{
 			return false;
 		
 		int remaining = claim.onFufill(item.stackSize);
-		if(remaining == 0)
+		if (remaining == 0)
 			return true;
 		
 		ItemStack stack = new ItemStack(item.getItem(), remaining, item.getMetadata());
